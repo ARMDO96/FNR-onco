86e2aa581652e3c5bcd2ab6896a57a51b35cd36e
ARMDO96 <a.rmdo96@gmail.com>
2026-09-24 12:57:36 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
-const CACHE='fnr-onco-v2';
+const CACHE='fnr-onco-v3';
 const FILES=['./','index.html','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png','icons/maskable-512.png',
-'fonts/ibm-plex-sans-latin-400-normal.woff2','fonts/ibm-plex-sans-latin-500-normal.woff2','fonts/ibm-plex-sans-latin-600-normal.woff2',
-'fonts/ibm-plex-sans-condensed-latin-500-normal.woff2','fonts/ibm-plex-sans-condensed-latin-600-normal.woff2',
-'fonts/ibm-plex-mono-latin-400-normal.woff2','fonts/ibm-plex-mono-latin-500-normal.woff2'];
+'fonts/lexend-latin-400-normal.woff2','fonts/lexend-latin-500-normal.woff2','fonts/lexend-latin-600-normal.woff2','fonts/figtree-latin-400-normal.woff2','fonts/figtree-latin-500-normal.woff2','fonts/figtree-latin-600-normal.woff2','fonts/figtree-latin-700-normal.woff2','fonts/ibm-plex-mono-latin-400-normal.woff2','fonts/ibm-plex-mono-latin-500-normal.woff2'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
